732a414da254e3519c01e0b36c0388b033967c23
LilFumanal <charlie.fumanal@gmail.com>
2024-06-20 04:33:02 +0200

    settings

--- SafeTag/settings.py
+++ SafeTag/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = env("SECRET_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
