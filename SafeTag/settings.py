c1b0f1ca64c1b90e90eb15882885546469e4c13f
LilFumanal <charlie.fumanal@gmail.com>
2024-06-23 03:38:53 +0200

    Cors policy

--- SafeTag/settings.py
+++ SafeTag/settings.py
@@ -31,9 +31,18 @@ if IS_HEROKU_APP:
 else:
     ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0"]
 # Application definition
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",  # Your Angular app's URL during development
+    "https://your-production-domain.com",  # Your app's production URL
+]
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "authorization",
+    "x-csrftoken",
+]
 INSTALLED_APPS = [
     "SafeTagAPI",
+    'corsheaders',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -43,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
